d3dcba4fc723099591a1fe3399797b8b232bbe7f
adriano515 <valdezadriano@gmail.com>
2015-09-07 20:28:41 -0600

    clasificador

---
+++ HojadeTrabajo7/src/Clasi.java
@@ -0,0 +1,23 @@
+import java.util.Hashtable;
+
+// Juan Luis Garcia
+// Freddie Batlle
+// Clase para ordenar las cadenas
+
+public class Clasi{
+	
+	Hashtable<Character,Integer> table = new Hashtable<Character,Integer>();
+	
+	public void cadena(String str){
+	
+		char[] cadena= new char[str.length()];
+				cadena = str.toCharArray();
+		
+	for(char c : cadena ) {
+	    if( table.get(c) == null )
+	        table.put(c,1);
+	    else
+	        table.put(c,table.get(c) + 1);
+	}
+	}
+}
